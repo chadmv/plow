644682fa70a7e3a75ccf343434806747e928acd5
sqlboy <mvchambers@me.com>
2012-09-19 20:21:55 +1200

    move logging config to top of file

--- tools/rnd.py
+++ tools/rnd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import logging
+logging.basicConfig(level=logging.INFO)
+
 import sys
 sys.path.append("../client/python")
 
 import rndlib.server
-
-logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     rndlib.server.start()
