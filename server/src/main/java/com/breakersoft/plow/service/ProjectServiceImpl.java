babe926ac0b36de15bac76f62066751db6a29cb4
sqlboy <mvchambers@me.com>
2012-09-20 21:42:17 +1200

    Added a project service.

---
+++ server/src/main/java/com/breakersoft/plow/service/ProjectServiceImpl.java
@@ -0,0 +1,28 @@
+package com.breakersoft.plow.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.breakersoft.plow.Defaults;
+import com.breakersoft.plow.Folder;
+import com.breakersoft.plow.Project;
+import com.breakersoft.plow.dao.FolderDao;
+import com.breakersoft.plow.dao.ProjectDao;
+
+@Service
+@Transactional
+public class ProjectServiceImpl implements ProjectService {
+
+    @Autowired
+    ProjectDao projectDao;
+
+    @Autowired
+    FolderDao folderDao;
+
+    public Project createProject(String name, String title) {
+        Project project = projectDao.create(name, title);
+        return project;
+    }
+
+}
