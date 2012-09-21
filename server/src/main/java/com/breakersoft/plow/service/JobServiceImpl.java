4135db40a95b29a220b125ee17e1cf9678ae43df
sqlboy <mvchambers@me.com>
2012-09-22 09:27:50 +1200

    placeholder for jobService bean

---
+++ server/src/main/java/com/breakersoft/plow/service/JobServiceImpl.java
@@ -0,0 +1,17 @@
+package com.breakersoft.plow.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.breakersoft.plow.Job;
+import com.breakersoft.plow.dao.JobDao;
+import com.breakersoft.plow.dispatcher.DispatchJob;
+
+@Service
+@Transactional
+public class JobServiceImpl implements JobService {
+
+
+
+}
