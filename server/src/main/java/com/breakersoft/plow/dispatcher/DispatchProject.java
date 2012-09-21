26ccd3a6348c17b87a4d590df65f8ec572dffa43
sqlboy <mvchambers@me.com>
2012-09-22 11:33:05 +1200

    Added a DispatchProject object for sorting on project furthest from its quota size.

---
+++ server/src/main/java/com/breakersoft/plow/dispatcher/DispatchProject.java
@@ -0,0 +1,20 @@
+package com.breakersoft.plow.dispatcher;
+
+import com.breakersoft.plow.ProjectE;
+
+public class DispatchProject extends ProjectE {
+
+    float tier;
+
+    public DispatchProject() {
+
+    }
+
+    public void setTier(float tier) {
+        this.tier = tier;
+    }
+
+    public float getTier() {
+        return this.tier;
+    }
+}
