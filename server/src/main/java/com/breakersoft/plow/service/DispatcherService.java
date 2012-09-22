a9d8767e9d4bc5c1b8fbe15e38033ed58395071c
sqlboy <mvchambers@me.com>
2012-09-22 21:22:29 +1200

    Updated dispatcher service with getDispatchNode

--- server/src/main/java/com/breakersoft/plow/service/DispatcherService.java
+++ server/src/main/java/com/breakersoft/plow/service/DispatcherService.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import com.breakersoft.plow.Folder;
 import com.breakersoft.plow.Job;
-import com.breakersoft.plow.Project;
 import com.breakersoft.plow.dispatcher.DispatchFolder;
 import com.breakersoft.plow.dispatcher.DispatchJob;
 import com.breakersoft.plow.dispatcher.DispatchNode;
+import com.breakersoft.plow.dispatcher.DispatchProject;
 
 public interface DispatcherService {
 
-    List<Project> getSortedProjectList(DispatchNode node);
+    List<DispatchProject> getSortedProjectList(DispatchNode node);
 
     DispatchJob getDispatchJob(Job job);
 
     DispatchFolder getDispatchFolder(Folder folder);
+
+    DispatchNode getDispatchNode(String name);
 }
--- server/src/main/java/com/breakersoft/plow/service/DispatcherServiceImpl.java
+++ server/src/main/java/com/breakersoft/plow/service/DispatcherServiceImpl.java
@@ -2,27 +2,29 @@ package com.breakersoft.plow.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.breakersoft.plow.Folder;
 import com.breakersoft.plow.Job;
-import com.breakersoft.plow.Project;
 import com.breakersoft.plow.dao.DispatchDao;
 import com.breakersoft.plow.dispatcher.DispatchFolder;
 import com.breakersoft.plow.dispatcher.DispatchJob;
 import com.breakersoft.plow.dispatcher.DispatchNode;
+import com.breakersoft.plow.dispatcher.DispatchProject;
 
 @Service
 @Transactional
 public class DispatcherServiceImpl implements DispatcherService {
 
+    @Autowired
     private DispatchDao dispatchDao;
 
     @Override
-    public List<Project> getSortedProjectList(DispatchNode node) {
-        // TODO Auto-generated method stub
-        return null;
+    @Transactional(readOnly=true)
+    public List<DispatchProject> getSortedProjectList(DispatchNode node) {
+        return dispatchDao.getSortedProjectList(node);
     }
 
     @Override
@@ -35,5 +37,11 @@ public class DispatcherServiceImpl implements DispatcherService {
     @Transactional(readOnly=true)
     public DispatchFolder getDispatchFolder(Folder folder) {
         return dispatchDao.getDispatchFolder(folder);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public DispatchNode getDispatchNode(String name) {
+        return dispatchDao.getDispatchNode(name);
     }
 }
