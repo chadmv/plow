176f1ec48835e4474342523f1170976f175cf3aa
sqlboy <mvchambers@me.com>
2012-10-21 09:31:13 +1300

    Updated RndClient to use TFramedTransport.

--- server/src/main/java/com/breakersoft/plow/thrift/RndClient.java
+++ server/src/main/java/com/breakersoft/plow/thrift/RndClient.java
@@ -3,7 +3,9 @@ package com.breakersoft.plow.thrift;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 
@@ -20,7 +22,8 @@ public class RndClient {
     private final String host;
     private final int port;
 
-    private TSocket transport;
+    private TSocket socket;
+    private TTransport transport;
     private TProtocol protocol;
 
     public RndClient(String host, int port) {
@@ -29,8 +32,9 @@ public class RndClient {
     }
 
     public RndNodeApi.Client connect() throws TTransportException {
-        transport = new TSocket(host, port);
-        protocol = new TBinaryProtocol(transport);
+        socket = new TSocket(host, port);
+        transport = new TFramedTransport(socket);
+        protocol = new TBinaryProtocol(socket);
         transport.open();
         return new RndNodeApi.Client(protocol);
     }
@@ -42,7 +46,7 @@ public class RndClient {
             throw new RndClientExecuteException(e);
         }
         finally {
-            transport.close();
+            socket.close();
         }
     }
 }
