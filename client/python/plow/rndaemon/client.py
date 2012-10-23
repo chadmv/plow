530289ae1308105231a7414fb1f39ce696cbfad2
Justin Israel <justinisrael@gmail.com>
2012-10-22 19:45:06 -0700

    rndaemon: added/reorganized new rndaemon tests expanded server/client modules to get server/connection objects with options (for test integration) commit references #2

--- client/python/plow/rndaemon/client.py
+++ client/python/plow/rndaemon/client.py
@@ -6,8 +6,12 @@ from thrift.protocol import TBinaryProtocol
 
 from rpc import RndServiceApi, RndNodeApi
 
-def getPlowConnection():
-    host, port = conf.PLOW_HOSTS[0].split(":")
+def getPlowConnection(host=None, port=None):
+    conf_host, conf_port = conf.PLOW_HOSTS[0].split(":")
+    if host is None:
+        host = conf_host
+    if port is None:
+        port = conf_port
     socket = TSocket.TSocket(host, int(port))
     transport = TTransport.TFramedTransport(socket)
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
@@ -19,6 +23,6 @@ def getLocalConnection():
     socket = TSocket.TSocket("localhost", conf.NETWORK_PORT)
     transport = TTransport.TFramedTransport(socket)
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    service = RndProcessApi.Client(protocol)
+    service = RndNodeApi.Client(protocol)
     transport.open()
     return service
