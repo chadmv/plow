892d0db712b831c185dde791c2a1c8d37e03af8f
sqlboy <mvchambers@me.com>
2012-10-28 20:45:28 +1300

    New class for handling aggregate Rndaemon functions.

---
+++ server/src/main/java/com/breakersoft/plow/service/RndProcessManager.java
@@ -0,0 +1,45 @@
+package com.breakersoft.plow.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.breakersoft.plow.Job;
+import com.breakersoft.plow.Proc;
+import com.breakersoft.plow.exceptions.RndClientExecuteException;
+import com.breakersoft.plow.rndaemon.RndClient;
+
+/**
+ * Component for handling operations that require talking to
+ * multiple render nodes.
+ *
+ * @author chambers
+ *
+ */
+@Component
+public class RndProcessManager {
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(RndProcessManager.class);
+
+    @Autowired
+    NodeService nodeService;
+
+    public void killProcs(Job job, String reason) {
+        final List<Proc> procs = nodeService.getProcs(job);
+
+        for (Proc proc: procs) {
+            nodeService.setProcUnbooked(proc, true);
+
+            RndClient client = new RndClient(proc.getHostname());
+            try {
+                client.kill(proc, reason);
+            }
+            catch (RndClientExecuteException e) {
+                logger.warn("Failed to kill process {} on {} ({}), {}",
+                        new Object[] { proc.getProcId(), proc.getHostname(), reason, e });
+            }
+        }
+    }
+}
