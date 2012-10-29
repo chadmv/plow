442893b24231ab29ba95cf3039d43d24ce8cf888
sqlboy <mvchambers@me.com>
2012-10-29 23:00:25 +1300

    Added DAO methods to allocate and free resources from a node.

--- server/src/test/java/com/breakersoft/plow/test/dao/NodeDaoTests.java
+++ server/src/test/java/com/breakersoft/plow/test/dao/NodeDaoTests.java
@@ -1,13 +1,18 @@
 package com.breakersoft.plow.test.dao;
 
+import static org.junit.Assert.*;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 
 import com.breakersoft.plow.Cluster;
+import com.breakersoft.plow.Defaults;
 import com.breakersoft.plow.Node;
 import com.breakersoft.plow.dao.ClusterDao;
 import com.breakersoft.plow.dao.NodeDao;
+import com.breakersoft.plow.exceptions.ResourceAllocationException;
+import com.breakersoft.plow.rnd.thrift.Ping;
 import com.breakersoft.plow.test.AbstractTest;
 
 public class NodeDaoTests extends AbstractTest {
@@ -20,10 +25,53 @@ public class NodeDaoTests extends AbstractTest {
 
     @Test
     public void create() {
+        Ping ping = getTestNodePing();
+        Cluster cluster = clusterDao.create("test", "test");
+        Node node = nodeDao.create(cluster, ping);
+        assertEquals(cluster.getClusterId(), node.getClusterId());
+        assertEquals(ping.getHostname(), node.getName());
+    }
 
+    @Test
+    public void allocateResources() {
+        Ping ping = getTestNodePing();
+        Cluster cluster = clusterDao.create("test", "test");
+        Node node = nodeDao.create(cluster, ping);
+        nodeDao.allocateResources(node, 1, 1024);
+
+        // Check to ensure the procs/memory were subtracted from the host.
+        assertEquals(ping.hw.physicalCpus - 1,
+                simpleJdbcTemplate.queryForInt("SELECT int_free_cores FROM node_dsp WHERE pk_node=?",
+                        node.getNodeId()));
+
+        assertEquals(ping.hw.totalRamMb - 1024 - Defaults.MEMORY_RESERVE_MB,
+                simpleJdbcTemplate.queryForInt("SELECT int_free_memory FROM node_dsp WHERE pk_node=?",
+                        node.getNodeId()));
+
+    }
+
+    @Test(expected=ResourceAllocationException.class)
+    public void allocateResourcesFailed() {
         Cluster cluster = clusterDao.create("test", "test");
         Node node = nodeDao.create(cluster, getTestNodePing());
+        nodeDao.allocateResources(node, 100, 1000000);
+    }
 
+    @Test
+    public void freeResources() {
 
+        Ping ping = getTestNodePing();
+        Cluster cluster = clusterDao.create("test", "test");
+        Node node = nodeDao.create(cluster, ping);
+        nodeDao.allocateResources(node, 1, 1024);
+        nodeDao.freeResources(node, 1, 1024);
+
+        assertEquals(ping.hw.physicalCpus,
+                simpleJdbcTemplate.queryForInt("SELECT int_free_cores FROM node_dsp WHERE pk_node=?",
+                        node.getNodeId()));
+
+        assertEquals(ping.hw.totalRamMb - Defaults.MEMORY_RESERVE_MB,
+                simpleJdbcTemplate.queryForInt("SELECT int_free_memory FROM node_dsp WHERE pk_node=?",
+                        node.getNodeId()));
     }
 }
