d6c13e15f6701edaa09433bddc56551339b5561e
sqlboy <mvchambers@me.com>
2012-11-03 18:06:53 +1300

    Converted plow config over to new system.

--- lib/python/plow/conf.py
+++ lib/python/plow/conf.py
@@ -4,42 +4,34 @@ import ConfigParser
 
 __all__ = ["Config"]
 
-Config = ConfigParser.RawConfigParser()
-
+_Config = ConfigParser.RawConfigParser()
+_Args = { }
 
 def _init():
     """
     Parse an initalize the Config object
     """
     if os.environ.get("PLOW_CFG"):
-        Config.read([os.environ["PLOW_CFG"]])
+        _Config.read([os.environ["PLOW_CFG"]])
     else:
-        Config.read(["/etc/plow/plow.cfg", os.path.expanduser("~/.plow/plow.cfg")])
-
-    root = os.environ.get("PLOW_ROOT")
-    if not root:
-        root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
-
-    if not 'env' in Config.sections():
-        Config.add_section('env')
-        
-    Config.set('env', 'plow_root', root)
+        _Config.read([
+            os.path.join(os.environ.get("PLOW_ROOT", "/usr/local"), "etc/plow/plow.cfg"),
+            os.path.expanduser("~/.plow/plow.cfg")])
 
 # run as a function to avoid polluting module with temp variables
 _init()
 
-
-assert Config.has_section("plow")
+assert _Config.has_section("plow")
 
 def get(section, key):
     """
     Return the specified configuration option.  Automatically
     interpolates any environement variables specified in plow.ini.
     """
-    interps = Config.get("env", "interpolate").split(",")
+    interps = _Config.get("env", "interpolate").split(",")
     args = dict([(inter, os.environ.get(inter, "test")) for inter in interps])
 
-    result = Config.get(section, key)
+    result = _Config.get(section, key)
     return result % args
 
 
