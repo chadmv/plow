6b06cc2d01a45f9eea8ab6c2ebbdff47571314b8
sqlboy <mvchambers@me.com>
2012-11-04 20:47:53 +1300

    Fixed bugs with job spec generation, needed full path to taskrun.

--- lib/python/blueprint/backend/plowrun.py
+++ lib/python/blueprint/backend/plowrun.py
@@ -3,7 +3,6 @@ import yaml
 import getpass
 
 import plow
-print dir(plow)
 import blueprint.conf as conf
 
 def launch(runner):
@@ -19,13 +18,11 @@ def serialize(runner):
     """
     Convert the job from the internal blueprint stucture to a plow JobSpec.
     """
-
-    base_name = runner.getJob().getName()
-    job_name = conf.get("templates", "job_name", {"JOB_NAME": base_name})
-    log_dir = conf.get("templates", "log_dir", {"JOB_NAME": base_name})
-
     job = runner.getJob()
-
+    base_name = job.getName()
+    job_name = conf.get("templates", "job_name", JOB_NAME=base_name)
+    log_dir = conf.get("templates", "log_dir", JOB_NAME=base_name)
+    
     spec = plow.JobSpecT()
     spec.project = os.environ.get("PLOW_PROJECT",
         conf.get("defaults", "project"))
@@ -39,15 +36,15 @@ def serialize(runner):
     for layer in job.getLayers():
         lspec = plow.LayerSpecT()
         lspec.name = layer.getName()
-        lspec.command =[
-            os.path.join("taskrun"),
+        lspec.command = [
+            "%s/bin/taskrun" % os.environ.get("PLOW_ROOT", "/usr/local"),
             os.path.join(job.getPath(), "blueprint.yaml"),
             "-layer",
             layer.getName(),
             "-range",
             "%{FRAME}"
         ]
-
+        
         lspec.tags =  layer.getArg("tags", ["unassigned"])
         lspec.range = layer.getArg("frame_range", runner.getArg("frame_range", "1001"))
         lspec.chunk = layer.getArg("chunk", 1)
