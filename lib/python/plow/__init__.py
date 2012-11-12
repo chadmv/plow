ec43668afadfe4b575713f9c871b8eb7813114bd
sqlboy <mvchambers@me.com>
2012-11-12 21:09:05 +1300

    Import a bunch of shit into the package namespace.

--- lib/python/plow/__init__.py
+++ lib/python/plow/__init__.py
@@ -1,3 +1,5 @@
 from static import *
 
-from rpc.ttypes import JobSpecT, LayerSpecT, JobState, TaskState
+from rpc.ttypes import JobSpecT, LayerSpecT, DependSpecT
+from rpc.ttypes import JobState, TaskState, NodeState, LockState
+from rpc.ttypes import DependType
