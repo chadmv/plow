fd49baef04d93db075d9d80de863b64a9884c0eb
Justin Israel <justinisrael@gmail.com>
2012-11-12 13:34:44 -0800

    gui/NodeModel: using simpler template and letting Qt handle enum name lookup

--- lib/cpp/src/gui/NodeModel.h
+++ lib/cpp/src/gui/NodeModel.h
@@ -3,21 +3,11 @@
 
 #include <vector>
 #include <QStandardItemModel>
-#include <QMap>
-#include <QPair>
+#include <QMetaEnum>
 
 #include "plow_types.h"
 
 namespace Plow { namespace Gui {
-
-
-template <class Key, class T> class InitializableQMap : public QMap<Key,T>
-{
-public:
-    inline InitializableQMap<Key,T> &operator<< (const QPair<Key,T> &t)
-    { this->insert(t.first,t.second); return *this; }
-};
-
 
 //
 // NodeModel
@@ -27,21 +17,38 @@ typedef std::vector<Plow::NodeT> NodeList;
 class NodeModel : public QStandardItemModel
 {
     Q_OBJECT
+    Q_ENUMS(NODE_STATE LOCK_STATE)
 
 public:
     NodeModel(QObject *parent = 0);
 
-    static const InitializableQMap<NodeState::type, QString> NODE_STATES;
-    static const InitializableQMap<LockState::type, QString> LOCK_STATES;
+    enum NODE_STATE {
+    	UP=NodeState::UP, 
+    	DOWN=NodeState::DOWN, 
+    	REPAIR=NodeState::REPAIR, 
+    	REBOOT=NodeState::REBOOT
+    };
 
-signals:
-    
+    enum LOCK_STATE {
+    	OPEN=LockState::OPEN, 
+    	LOCKED=LockState::LOCKED
+    };
+
+    template<typename T>
+    QString enumToString(const char*, T) const;
+
+
 public slots:
     void populate();
 
-
 };
 
+template<typename T>
+inline QString NodeModel::enumToString(const char* enumName, T enumVal) const {
+    int index = staticMetaObject.indexOfEnumerator(enumName);
+    QMetaEnum metaEnum = staticMetaObject.enumerator(index);
+    return QString(metaEnum.valueToKey(enumVal));
+}
 
 } // Gui
 } // Plow
