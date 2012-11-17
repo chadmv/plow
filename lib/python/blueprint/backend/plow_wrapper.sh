583a96b3ca655542e2288ad3cfbaf4dfeb764f82
sqlboy <mvchambers@me.com>
2012-11-18 11:18:26 +1300

    Updated plow run module to handle latest features, depends, etc.

---
+++ lib/python/blueprint/backend/plow_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Wraps the execution of plow tasks on the render farm.
+#
+
+# Set a temporary directory for the process.
+export PLOW_TASK_TEMPDIR="/tmp/${PLOW_JOB_NAME}/${PLOW_TASK_NAME}"
+export TMPDIR="$PLOW_TASK_TMPDIR"
+
+# Make sure it exists
+mkdir -p ${TMPDIR}
+
+eval $@
+
+# Clean up the temp directory
+rm -f ${PLOW_TASK_TEMPDIR}/*
+
+
--- lib/python/blueprint/backend/plowrun.py
+++ lib/python/blueprint/backend/plowrun.py
@@ -4,9 +4,8 @@ import getpass
 import pprint
 
 import plow
+import blueprint
 import blueprint.conf as conf
-
-from blueprint import Layer
 
 def launch(runner):
     """
@@ -20,6 +19,18 @@ def launch(runner):
     else:
         job = plow.submitJobSpec(spec)
         runner.getJob().putData("jobid", job.id)
+
+
+def createLayerSpec(layer):
+    lspec = plow.LayerSpecT()
+    lspec.name = layer.getName()
+    lspec.tags =  layer.getArg("tags", ["unassigned"])
+    lspec.chunk = layer.getArg("chunk", 1)
+    lspec.minCores = layer.getArg("min_cores", 1)
+    lspec.maxCores = layer.getArg("max_cores", 0)
+    lspec.minMemory = layer.getArg("min_ram", 2048)
+
+    return lspec
 
 def serialize(runner):
     """
@@ -40,50 +51,111 @@ def serialize(runner):
     spec.logPath = log_dir
     spec.layers = []
 
+    # Task layers get created to store blueprint tasks.
+    task_layers = { }
+
     for layer in job.getLayers():
-        lspec = plow.LayerSpecT()
-        lspec.name = layer.getName()
-        lspec.tags =  layer.getArg("tags", ["unassigned"])
-        lspec.range = layer.getArg("frame_range", runner.getArg("frame_range", "1001"))
-        lspec.chunk = layer.getArg("chunk", 1)
-        lspec.minCores = layer.getArg("min_threads", 1)
-        lspec.maxCores = layer.getArg("max_threads", 0)
-        lspec.minMemory = layer.getArg("min_ram", 256)
 
-        lspec.command = [
-            "%s/bin/taskrun" % os.environ.get("PLOW_ROOT", "/usr/local"),
-            os.path.join(job.getPath(), "blueprint.yaml"),
-            "-layer",
-            layer.getName(),
-            "-range",
-            "%{FRAME}"
-        ]
+        if isinstance(layer, (blueprint.Task,)):
+
+            # Have to create a plow layer to store blueprint tasks.
+            # This would be to org
+            if not task_layers.has_key(layer.getArg("group")):
+                task_layer = createLayerSpec(layer)
+                task_layer.name = layer.getArg("group")
+                task_layer.tasks = []
+            else:
+                task_layer = task_layers[layer.getGroup()]
+                # Merge in the tags for the other layer.  Probably not the best option.
+                task_layer.tags.update(layer.getArg("tags", set()))
+                # Use the highest values on any task.
+                task_layer.minCores = max(task_layer.minCores, task.getArg("min_cores", 1))
+                task_layer.maxCores = max(task_layer.maxCores, task.getArg("max_cores", 0))
+                task_layer.minMemory = max(task_layer.minMemory, task.getArg("min_mem"))
         
-        lspec.depends = []
+            task_layer.command = [
+                "%s/plow_wrapper.sh" % os.path.dirname(__file__),
+                "%s/bin/taskrun" % os.environ.get("PLOW_ROOT", "/usr/local"),
+                os.path.join(job.getPath(), "blueprint.yaml"),
+                "-task",
+                "%{TASK}"
+            ]
 
-        for depend in layer.getDepends():
+            task = plow.TaskSpecT()
+            task.name = layer.getName()
+            task.depends = setupTaskDepends(job, layer) 
+            task_layer.tasks.append(task)
+            spec.layers.append(task_layer)
 
-            dspec = plow.DependSpecT()
-            if depend.type == layer.DependAll:
+        else:
+
+            lspec = createLayerSpec(layer)
+            lspec.depends = setupLayerDepends(job, layer)
+            lspec.range = layer.getArg("frame_range",
+                runner.getArg("frame_range", None))
+            lspec.command = [
+                "%s/plow_wrapper.sh" % os.path.dirname(__file__),
+                "%s/bin/taskrun" % os.environ.get("PLOW_ROOT", "/usr/local"),
+                os.path.join(job.getPath(), "blueprint.yaml"),
+                "-layer",
+                layer.getName(),
+                "-range",
+                "%{RANGE}"
+            ]
+            spec.layers.append(lspec)
+
+
+    return spec
+
+def setupLayerDepends(job, layer):
+    result = []
+    for depend in layer.getDepends():
+        dspec = plow.DependSpecT()
+        
+        depend_on = job.getLayer(str(depend.dependOn))
+        if isinstance(depend_on, (blueprint.Task,)):
+            # Layer on Task
+            dspec.type = plow.DependType.LAYER_ON_TASK
+            dspec.dependentLayer = str(depend.dependent)
+            dspec.dependOnTask = str(depend_on)
+        else:
+            # Layer on Layer + Task by Task
+            if depend.type == blueprint.DependType.All:
                 dspec.type = plow.DependType.LAYER_ON_LAYER
-            elif depend.type == layer.DependByTask:
+            elif depend.type == blueprint.DependType.ByTask:
                 dspec.type = plow.DependType.TASK_BY_TASK
             else:
                 raise Exception("Invalid layer depend type: %s"  % depend.type)
+            dspec.dependentLayer = str(depend.dependent)
+            dspec.dependOnLayer = str(depend.dependOn)
 
-            if isinstance(depend.dependent, (Layer,)):
-                dspec.dependentLayer = depend.dependent.getName()
-            else:
-                dspec.dependentLayer = str(depend.dependent)
+        result.append(dspec)
+    return result
 
-            if isinstance(depend.dependOn, (Layer,)):
-                dspec.dependOnLayer = depend.dependOn.getName()
-            else:
-                dspec.dependOnLayer = str(depend.dependOn)
 
-            lspec.depends.append(dspec)
+def setupTaskDepends(job, task):
+    """
+    Setup task dependencies.  Tasks can depend on other
+    tasks or layers.
+    """
+    result = []
+    for depend in task.getDepends():
+        dspec = plow.DependSpecT()
+        depend_on = job.getLayer(str(depend.dependOn))
+        if isinstance(depend_on, (blueprint.Task,)):
+            # Task on Task
+            dspec.type = plow.DependType.TASK_ON_TASK
+            dspec.dependentTask = str(task)
+            dspec.dependOnTask = str(depend_on)
+        else:
+            # Task on Layer
+            dspec.type = plow.DependType.TASK_ON_LAYER    
+            dspec.dependentTask = str(task)
+            dspec.dependOnLayer = str(depend.dependOn)
+        result.append(dspec)
+    return result
 
-        spec.layers.append(lspec)
 
-    return spec
+
+
 
