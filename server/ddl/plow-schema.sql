a32da41d4031f27c9c439d94334013396b996ffd
sqlboy <mvchambers@me.com>
2012-11-17 23:29:31 +1300

    Merged SQL into 1 file

--- server/ddl/plow-schema.sql
+++ server/ddl/plow-schema.sql
@@ -168,7 +168,7 @@ CREATE TABLE plow.task (
 CREATE INDEX task_pk_layer_idx ON plow.task (pk_layer);
 CREATE INDEX task_pk_job_idx ON plow.task (pk_job);
 CREATE INDEX task_int_state_idx ON plow.task (int_state);
-
+CREATE UNIQUE INDEX task_str_name_pk_job_idx_uniq ON plow.task (str_name, pk_job);
 ----------------------------------------------------------
 
 ---
@@ -297,4 +297,94 @@ CREATE INDEX proc_pk_quota_idx ON plow.proc (pk_quota);
 CREATE INDEX proc_pk_node_idx ON plow.proc (pk_node);
 CREATE INDEX proc_pk_task_idx ON plow.proc (pk_task);
 CREATE INDEX proc_pk_job_idx ON plow.proc (pk_job);
+
+----------------------------------------------------------
+
+---
+--- plow.after_task_state_change()
+---
+--- Handle incrementing/decrementating the frame state counters.
+---
+CREATE OR REPLACE FUNCTION plow.after_task_state_change() RETURNS TRIGGER AS $$
+DECLARE
+    old_state_col VARCHAR;
+    new_state_col VARCHAR;
+    states VARCHAR[];
+BEGIN
+
+  states := ARRAY['INITIALIZE', 'WAITING', 'RUNNING', 'DEAD', 'EATEN' , 'DEPEND', 'SUCCEEDED'];
+  old_state_col := 'int_' || lower(states[old.int_state + 1]);
+  new_state_col := 'int_' || lower(states[new.int_state + 1]);
+
+  EXECUTE 'UPDATE plow.layer_count SET ' || old_state_col || '=' || old_state_col || ' -1, '
+            || new_state_col || '=' || new_state_col || '+1 WHERE pk_layer=$1' USING new.pk_layer;
+
+  EXECUTE 'UPDATE plow.job_count SET ' || old_state_col || '=' || old_state_col || ' -1, '
+            || new_state_col || '=' || new_state_col || '+1 WHERE pk_job='
+            || '(SELECT pk_job FROM plow.layer WHERE pk_layer=$1)' USING new.pk_layer;
+
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trig_after_task_state_change AFTER UPDATE ON plow.task
+    FOR EACH ROW WHEN (OLD.int_state != 0 AND OLD.int_state != NEW.int_state)
+    EXECUTE PROCEDURE plow.after_task_state_change();
+
+---
+--- plow.before_task_depend_check()
+---
+--- Before task dependency check. Runs if the task has a depend count
+--- greater than zero and flips the state to depend.
+---
+CREATE OR REPLACE FUNCTION plow.before_update_set_depend() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.int_state := 5;
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trig_before_update_set_depend BEFORE UPDATE ON plow.task
+    FOR EACH ROW WHEN (NEW.int_depend_count > 0 AND NEW.int_state=1)
+    EXECUTE PROCEDURE plow.before_update_set_depend();
+
+---
+--- plow.before_update_task_depend_check()
+---
+--- Before task dependency check. Runs if the task has a depend count
+--- greater than zero and flips the state to depend.
+---
+CREATE OR REPLACE FUNCTION plow.before_update_set_waiting() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.int_state := 1;
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trig_before_update_set_waiting BEFORE UPDATE ON plow.task
+    FOR EACH ROW WHEN (NEW.int_depend_count=0 AND NEW.int_state=5)
+    EXECUTE PROCEDURE plow.before_update_set_waiting();
+
+
+----------------------------------------------------------
+
+
+---
+--- Test Project
+---
+INSERT INTO plow.project VALUES ('00000000-0000-0000-0000-000000000000', null, 'test', 'The Test Project');
+INSERT INTO plow.folder VALUES ('00000000-0000-0000-0000-000000000000', null, '00000000-0000-0000-0000-000000000000', 'All Jobs');
+INSERT INTO folder_dsp VALUES ('00000000-0000-0000-0000-000000000000', -1, 0, 0);
+UPDATE plow.project SET pk_folder_default = '00000000-0000-0000-0000-000000000000' WHERE pk_project='00000000-0000-0000-0000-000000000000';
+
+---
+--- Test Cluster
+---
+INSERT INTO plow.cluster VALUES ('00000000-0000-0000-0000-000000000000', 'unassigned', 'unassigned', 'f', 't');
+
+INSERT INTO plow.quota VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000','00000000-0000-0000-0000-000000000000', 10, 20, 0, 'f');
+
 
