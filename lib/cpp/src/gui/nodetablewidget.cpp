df12d16c9bbd62da0b7db0aae946201d7eaa993f
Justin Israel <justinisrael@gmail.com>
2012-11-17 21:53:15 -0800

    nodewidget: add proxy as a member of table and set to wrap a NodeModel Created a ResourceDelegate to show percentages between columns (prep for graphical meter) using a tab view for examples

--- lib/cpp/src/gui/nodetablewidget.cpp
+++ lib/cpp/src/gui/nodetablewidget.cpp
@@ -9,31 +9,77 @@
 namespace Plow {
 namespace Gui {
 
+//
+// NodeTableWidget
+//
 NodeTableWidget::NodeTableWidget(QWidget *parent)
     : QWidget(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout;
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    // default model
+    proxyModel = new NodeProxyModel(this);
+    proxyModel->setSourceModel(new NodeModel(this));
 
     tableView = new QTableView(this);
     tableView->verticalHeader()->hide();
     tableView->setEditTriggers(tableView->NoEditTriggers);
     tableView->setSelectionBehavior(tableView->SelectRows);
     tableView->setSortingEnabled(true);
-    tableView->sortByColumn(0, Qt::AscendingOrder);
 
-    // default model
-    tableView->setModel(new NodeModel(this));
+    tableView->sortByColumn(0, Qt::AscendingOrder);
+    tableView->setModel(proxyModel);
+
+    tableView->setColumnHidden(8, true); // total RAM
+    tableView->setColumnHidden(10, true); // total SWAP
 
     layout->addWidget(tableView);
-    setLayout(layout);
+
+    // Map free ram to total ram
+    tableView->setItemDelegateForColumn(9, new ResourceDelegate(8, this));
+    // Map free swap to total swap
+    tableView->setItemDelegateForColumn(11, new ResourceDelegate(10, this));
 }
 
-QAbstractItemModel* NodeTableWidget::model() const {
-    return tableView->model();
+NodeModel* NodeTableWidget::model() const {
+    return qobject_cast<NodeModel*>(proxyModel->sourceModel());
 }
 
-void NodeTableWidget::setModel(QAbstractItemModel* aModel) {
-    tableView->setModel(aModel);
+void NodeTableWidget::setModel(NodeModel *aModel) {
+    proxyModel->setSourceModel(aModel);
 }
+
+//
+// ResourceDelegate
+//
+ResourceDelegate::ResourceDelegate(int totalColumn, QObject *parent)
+    : QItemDelegate(parent)
+{
+    this->totalColumn = totalColumn;
+}
+
+void ResourceDelegate::paint(QPainter *painter,
+                             const QStyleOptionViewItem &option,
+                             const QModelIndex &index) const
+{
+    QVariant totalData = index.model()->index(index.row(), totalColumn).data();
+    QVariant currentData = index.data();
+    if (totalData.canConvert<double>()
+            && currentData.canConvert<double>()) {
+
+        QString text = QString("%1%")
+                .arg((currentData.toDouble() / totalData.toDouble()) * 100,
+                     5, 'f', 1);
+
+        QStyleOptionViewItem opt = option;
+        opt.displayAlignment = Qt::AlignCenter;
+
+        drawDisplay(painter, opt, opt.rect, text);
+
+    } else {
+        QItemDelegate::paint(painter, option, index);
+    }
+}
+
 }  // Gui
 }  // Plow
--- lib/cpp/src/gui/nodetablewidget.h
+++ lib/cpp/src/gui/nodetablewidget.h
@@ -2,13 +2,19 @@
 #define PLOW_GUI_NODETABLEWIDGET_H_
 
 #include <QWidget>
+#include <QItemDelegate>
 
 class QTableView;
-class QAbstractItemModel;
 
 namespace Plow {
 namespace Gui {
 
+class NodeModel;
+class NodeProxyModel;
+
+//
+// NodeTableWidget
+//
 class NodeTableWidget
         : public QWidget
 {
@@ -17,12 +23,32 @@ class NodeTableWidget
  public:
     explicit NodeTableWidget(QWidget *parent = 0);
 
-    QAbstractItemModel* model() const;
-    void setModel(QAbstractItemModel* aModel);
+    NodeModel* model() const;
+    void setModel(NodeModel *aModel);
 
  private:
     QTableView* tableView;
+    NodeProxyModel* proxyModel;
 };
+
+//
+// ResourceDelegate
+//
+class ResourceDelegate : public QItemDelegate
+{
+    Q_OBJECT
+
+ public:
+    ResourceDelegate(int totalColumn, QObject *parent = 0);
+
+    void paint(QPainter *painter,
+               const QStyleOptionViewItem &option,
+               const QModelIndex &index) const;
+
+ private:
+    int totalColumn;
+};
+
 }  // Gui
 }  // Plow
 
