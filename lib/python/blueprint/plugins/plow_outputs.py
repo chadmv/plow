57b1512d058bc0ec63eb6c0106b5005f5d4e67ab
sqlboy <mvchambers@me.com>
2012-11-19 21:23:48 +1300

    Finished plow output plugin, which handles after exec event on SetupTasks and registers paths with plow.

--- lib/python/blueprint/plugins/plow_outputs.py
+++ lib/python/blueprint/plugins/plow_outputs.py
@@ -1,13 +1,30 @@
+"""
+Plow outputs plugin.
 
+Registers outputs with the plow server.
+"""
+import os
 import logging
-
 logger = logging.getLogger(__name__)
 
-def initLayer(layer):
-    pass
-    
-def initJob(job):
-    pass
+import blueprint
 
-def setup():
-    pass
+def afterExecute(layer):
+
+    import plow
+
+    # When a setup task is complete outputs should
+    # be registerd with its parent layer.
+    if not isinstance(layer, (blueprint.SetupTask,)):
+        return
+
+    parent = layer.getLayer()
+    logger.info("Registering %d outputs" % len(parent.getOutputs()))
+    for output in parent.getOutputs():
+        logger.info("Registering output with plow: %s" % output.path)
+        plow_layer = plow.getLayer(
+            os.environ.get("PLOW_JOB_ID"), parent.getName())
+        plow.registerOutput(plow_layer, output.path, {})
+
+
+
