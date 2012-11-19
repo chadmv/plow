0067a2f6ae77d6d223dfb583e2a312e3e196e1e1
sqlboy <mvchambers@me.com>
2012-11-19 21:22:52 +1300

    Added getLayer method.

--- lib/python/plow/static.py
+++ lib/python/plow/static.py
@@ -5,7 +5,9 @@ from client import Conn
 
 __all__ = [
     "submitJobSpec",
-    "findJobs"
+    "findJobs",
+    "getLayer",
+    "registerOutput"
 ]
 
 def submitJobSpec(jobspec):
@@ -16,5 +18,16 @@ def findJobs(**kwargs):
     for k, v in kwargs.items():
         setattr(filt, k, v)
     return Conn.service.getJobs(filt)
+
+def getLayer(*args):
+    if len(args) == 1:
+        return Conn.service.getLayerById(args[0])
+    elif len(args) == 2:
+        return Conn.service.getLayer(args[0], args[1])
+
+def registerOutput(layer, path, attrs=None):
+    if attrs is None:
+        attrs = dict()
+    Conn.service.addOutput(str(layer.id), str(path), attrs)
 
 
