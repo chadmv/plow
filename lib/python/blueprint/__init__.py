0887be1a9999567cd51a578adc67cb78bc1c8f0e
sqlboy <mvchambers@me.com>
2012-11-19 21:18:58 +1300

    Added TaskSpecT to python __init__

--- lib/python/blueprint/__init__.py
+++ lib/python/blueprint/__init__.py
@@ -2,7 +2,7 @@
 from app import PluginManager, Application, BlueprintRunner
 
 from job import Job
-from layer import Layer, Task, DependType, Depend
+from layer import Layer, Task, SetupTask, DependType, Depend
 
 
 PluginManager.loadAllPlugins()
--- lib/thrift/plow__init__.py
+++ lib/thrift/plow__init__.py
@@ -1,5 +1,5 @@
 from static import *
 
-from rpc.ttypes import JobSpecT, LayerSpecT, DependSpecT
+from rpc.ttypes import JobSpecT, LayerSpecT, TaskSpecT, DependSpecT
 from rpc.ttypes import JobState, TaskState, NodeState, LockState
 from rpc.ttypes import DependType
