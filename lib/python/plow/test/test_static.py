d07109e07e4d9fab488dfbbcf56fdfe18baa56ab
sqlboy <mvchambers@me.com>
2012-12-02 20:52:47 +1300

    Set the column count value based on size of header list.

--- lib/python/plow/test/test_static.py
+++ lib/python/plow/test/test_static.py
@@ -6,7 +6,20 @@ import plow
 class StaticModuletests(unittest.TestCase):
 
     def testFindJobs(self):
-        plow.findJobs()
+        plow.getJobs()
+
+
+    def testGetGroupedJobs(self):
+
+        result = [
+            {"id": 1, "parent":0, "name": "High"},
+            {"id": 2, "parent":1, "name": "Foo"}
+        ]
+
+        for p in result:
+            print p
+
+
 
 
 if __name__ == "__main__":
