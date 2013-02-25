3afc5695ed043f9329aea422e4ee0195f362301a
Matt Chambers <mvchambers@me.com>
2013-02-25 23:17:55 +1300

    Added a few more standard widgets.

--- lib/python/plow/gui/common/widgets.py
+++ lib/python/plow/gui/common/widgets.py
@@ -1,6 +1,20 @@
 """Non-Plow specific widgets."""
 from plow.gui.manifest import QtGui, QtCore
 
+class BooleanCheckBox(QtGui.QCheckBox):
+    def __init__(self, checked=True, parent=None):
+        QtGui.QCheckBox.__init__(self, parent)
+        self.setChecked(checked)
+
+    def setChecked(self, value):
+        if value:
+            self.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.setCheckState(QtCore.Qt.Unchecked)
+
+    def isChecked(self):
+        return self.checkState() == QtCore.Qt.Checked
+
 class FilterableListBox(QtGui.QWidget):
     """
     A list box widget with a text filter.
@@ -15,6 +29,60 @@ class FilterableListBox(QtGui.QWidget):
         layout.addWidget(self.text)
         layout.addWidget(self.items)
 
+
+class CheckableComboBox(QtGui.QWidget):
+    """
+    A combo box with selectable items.
+    """
+    pass
+
+class CheckableListBox(QtGui.QWidget):
+    """
+    A list box with selectable items.
+    """
+    def __init__(self, title, options, checked, allSelected=True, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        layout = QtGui.QVBoxLayout(self)
+
+        self.listItems = QtGui.QListWidget(self)
+        self.listItems.setMaximumHeight(100)
+
+        for opt in options:
+            item = QtGui.QListWidgetItem(opt)
+            item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            if opt in checked:
+                item.setCheckState(QtCore.Qt.Checked)
+            else:
+                item.setCheckState(QtCore.Qt.Unchecked)
+            self.listItems.addItem(item)
+
+        self.checkBoxAll = QtGui.QCheckBox("All %s" % title, self)
+        if allSelected:
+            self.listItems.setDisabled(True)
+            self.checkBoxAll.setCheckState(QtCore.Qt.Checked)
+        self.checkBoxAll.stateChanged.connect(self.__allSelectedToggled)
+
+        layout.addWidget(self.checkBoxAll)
+        layout.addWidget(self.listItems)
+
+    def isAllSelected(self):
+        return self.checkBoxAll.checkState() == QtCore.Qt.Checked
+
+    def getCheckedOptions(self):
+        result = []
+        if self.isAllSelected():
+            return result
+        for i in xrange(0, self.listItems.count()):
+            item = self.listItems.item(i)
+            if item.checkState() == QtCore.Qt.Checked:
+                result.append(str(item.text()))
+        return result
+
+    def __allSelectedToggled(self, state):
+        if state == QtCore.Qt.Checked:
+            self.listItems.setDisabled(True)
+        else:
+            self.listItems.setDisabled(False)
 
 class RadioBoxArray(QtGui.QWidget):
     """
