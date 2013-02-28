ec314d3d35872d58d70414d5f53691f48be109b9
Matt Chambers <mvchambers@me.com>
2013-03-01 07:57:20 +1300

    Added a simple event manager.

---
+++ lib/python/plow/gui/event.py
@@ -0,0 +1,25 @@
+
+
+class EventManager(object):
+
+    __Binds = { }
+
+    @classmethod
+    def bind(cls, name, function):
+        funcs = cls.__Binds.get(name)
+        if not funcs:
+            funcs = [function]
+            cls.__Binds[name] = funcs
+        else:
+            funcs.append(function)
+        print cls.__Binds
+
+    @classmethod
+    def emit(cls, name, *args, **kwargs):
+        funcs = cls.__Binds.get(name)
+        if not funcs:
+            return
+        for func in funcs:
+            func(*args, **kwargs)
+        print funcs
+
