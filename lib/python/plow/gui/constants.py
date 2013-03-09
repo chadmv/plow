c9685596b7654e7d63ad182e0dfaac22296945c3
Matt Chambers <mvchambers@me.com>
2013-03-10 00:46:21 +1300

    Color cleanups

--- lib/python/plow/gui/constants.py
+++ lib/python/plow/gui/constants.py
@@ -9,38 +9,28 @@ The job status is how the job is doin.
 
 """
 
-BLUE = QtGui.QColor(17, 103, 166)
-PURPLE = QtGui.QColor(166, 17, 125)
-RED = QtGui.QColor(166, 17, 34)
-ORANGE = QtGui.QColor(166, 85, 17)
-GREEN = QtGui.QColor(73, 166, 17)
-
+BLUE = QtGui.QColor(28, 182, 214)
+PURPLE = QtGui.QColor(214, 28, 205)
+RED = QtGui.QColor(214, 48, 28)
+ORANGE = QtGui.QColor(214, 94, 28)
+GREEN = QtGui.QColor(39, 214, 28)
+YELLOW = QtGui.QColor(214, 206, 28)
 GRAY = QtGui.QColor(66, 66, 66)
-
-COLOR_JOB_STATUS_PAUSED = QtGui.QColor(63, 79, 83)
-COLOR_JOB_STATUS_ERRORS = QtGui.QColor(97, 39, 39)
-
-COLOR_CLUSTER_LOCKED = BLUE
-COLOR_CLUSTER_REPAIR = QtGui.QColor(83, 80, 63)
-COLOR_CLUSTER_DOWN = QtGui.QColor(83, 80, 63)
-
-
 
 COLOR_JOB_STATE = [
     GRAY,
-    QtGui.QColor(53, 105, 24),
-    QtGui.QColor(37, 207, 44)
+    YELLOW.darker(),
+    GREEN.darker()
 ]
 
-ALPHA = 255
 COLOR_TASK_STATE = [
     GRAY,
-    QtGui.QColor(63, 79, 83),
-    QtGui.QColor(83, 80, 63),
-    QtGui.QColor(83, 80, 63),
-    QtGui.QColor(83, 63, 74),
-    QtGui.QColor(68, 63, 83),
-    QtGui.QColor(68, 83, 63)
+    BLUE,
+    YELLOW,
+    RED,
+    RED.darker(),
+    PURPLE,
+    GREEN
 ]
 
 TIME_NO_TIME = "__-__ __:__:__"
