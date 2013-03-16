7558d87e00b7df0f24996db6acf0a4782afaf557
Justin Israel <justinisrael@gmail.com>
2013-03-16 23:23:15 +1300

    simplify cython getClient() and start on Project wrapping

--- lib/cython/src/plow.pyx
+++ lib/cython/src/plow.pyx
@@ -1,56 +1,58 @@
-cimport cython
-cimport client
+from common_types cimport *
+from plow_types cimport *
+from client cimport getClient
+
+from libcpp.vector cimport vector
+
+
+def getPlowTime():
+    cdef int plowTime 
+    plowTime = getClient().proxy().getPlowTime()
+    return plowTime
+
 
 #######################
-# Client
+# Project
 #
+cdef initProject(ProjectT& p):
+    cdef Project project = Project(p.id, p.name, p.title)
+    return project
 
-@cython.internal
-cdef class PlowClient:
+
+cdef class Project:
+
+    cdef public str id, name, title
+
+    def __init__(self, str id="", str name="", str title=""):
+        self.id = id
+        self.name = name
+        self.title = title
+
+
+def get_project_by_id(Guid& guid):
+    cdef: 
+        ProjectT projT 
+        Project project
+
+    getClient().proxy().getProject(projT, guid)
+    project = initProject(projT)
+    return project
+
+
+def get_project_by_name(str name):
+    cdef: 
+        ProjectT projT 
+        Project project
+
+    getClient().proxy().getProjectByName(projT, name)
+    project = initProject(projT)
+    return project
+
+# def getProjects():
+#     cdef:
+#         vector[ProjectT] projects 
+#         vector[Project] results
+
+#     getClient().proxy().getProjects(projects)
+
     
-    cdef client.PlowClient* ptr
-
-    def __cinit__(self):
-        pass
-
-    def __init__(self):
-        pass   
-
-    cdef setClientPtr(self, client.PlowClient* ptr):
-        self.ptr = ptr
-
-    def proxy(self):
-        return 0
-
-
-cpdef getClient():
-    cdef:
-        client.PlowClient* ptr
-        PlowClient aClient 
-
-    ptr = client.getClient()
-    aClient = PlowClient()
-    aClient.setClientPtr(ptr)
-
-    return aClient
-
-
-# @cython.internal
-# cdef class Connection:
-    
-#   def __init__(self):
-#       pass
-
-#   def connect(self):
-#       pass
-
-#   def disconnect(self):
-#       pass
-
-#   def proxy(self):
-#       pass
-
-
-
-#
-#######################
