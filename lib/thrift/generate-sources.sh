7db2dd43192671d61aaeb7e4debd89331b40d880
Justin Israel <justinisrael@gmail.com>
2013-03-16 20:31:54 +1300

    more corrects to thrift bindings generation for cython

--- lib/thrift/generate-sources.sh
+++ lib/thrift/generate-sources.sh
@@ -38,20 +38,16 @@ mv $OUT/plow_*.h $EXP
 
 rm $OUT/RpcService_server.skeleton.cpp
 
-# C++
+# Cython
 ########################################################################
 
 OUT="../cython/src/core/rpc"
-EXP="../cython/src/export/plow"
 
 rm -rf $OUT
-mkdir $OUT
+mkdir -p $OUT
 
 thrift --gen cpp --out $OUT common.thrift
 thrift --gen cpp --out $OUT plow.thrift
-
-mv $OUT/common_*.h $EXP
-mv $OUT/plow_*.h $EXP
 
 rm $OUT/RpcService_server.skeleton.cpp
 
