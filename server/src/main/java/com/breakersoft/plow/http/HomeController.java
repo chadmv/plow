a8a4806f22e32914ae1802a79f87533bbed8804f
Matt Chambers <mvchambers@me.com>
2013-04-06 17:04:36 +1300

    Added an example servlet that converts the thrift object to json.

--- server/src/main/java/com/breakersoft/plow/http/HomeController.java
+++ server/src/main/java/com/breakersoft/plow/http/HomeController.java
@@ -1,8 +1,11 @@
 package com.breakersoft.plow.http;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,24 +19,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * @throws IOException
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! the client locale is "+ locale.toString());
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public void home(HttpServletResponse response) throws IOException {
+		response.getOutputStream().write("Welcome to plow\n".getBytes());
 	}
-	
+
 }
---
+++ server/src/main/java/com/breakersoft/plow/http/RestController.java
@@ -0,0 +1,51 @@
+package com.breakersoft.plow.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TSimpleJSONProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.breakersoft.plow.thrift.JobFilterT;
+import com.breakersoft.plow.thrift.JobT;
+import com.breakersoft.plow.thrift.dao.ThriftJobDao;
+
+/**
+ * A controller for exposing the REST interface.
+ *
+ * Collection URI: /plow/jobs
+ * Element URI: /plow/jobs/jobid
+ *
+ * @author chambers
+ *
+ */
+@Controller
+public class RestController {
+
+	private static final Logger logger = LoggerFactory.getLogger(RestController.class);
+
+    @Autowired
+    private ThriftJobDao thriftJobDao;
+
+	@RequestMapping(value = "/jobs", method = RequestMethod.GET)
+	public void jobs(HttpServletRequest request, HttpServletResponse response) throws IOException, TException {
+		final TSerializer serializer = new TSerializer(new TSimpleJSONProtocol.Factory());
+		final ServletOutputStream stream = response.getOutputStream();
+		response.setContentType("application/json");
+		stream.print("[\n");
+		for (JobT o: thriftJobDao.getJobs(new JobFilterT())) {
+			stream.print(serializer.toString(o));
+		}
+		stream.print("]");
+	}
+}
