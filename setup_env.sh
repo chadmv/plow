73ecac31fc6d43c38726705c2ae352ece602038c
Justin Israel <justinisrael@gmail.com>
2013-04-10 22:29:49 +1200

    don't set BLUEPRINT_CFG in plow setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -14,7 +14,6 @@ export PATH="$PATH:$PLOW_ROOT/bin"
 # Set config variables
 export PLOW_RNDAEMON_CFG="$PLOW_ROOT/etc/plow/rndaemon.cfg"
 export PLOW_CFG="$PLOW_ROOT/etc/plow/plow.cfg"
-export BLUEPRINT_CFG="$PLOW_ROOT/etc/plow/blueprint.cfg"
 
 # Set variables for blueprint job submission
 export PROJECT="default_proj"
