1b4fb51f0705658c29954ae4586d3c0646019424
Matt Chambers <mvchambers@me.com>
2013-04-18 21:36:08 +1200

    moved logger to static object

--- server/src/main/java/com/breakersoft/plow/service/RndProcessManager.java
+++ server/src/main/java/com/breakersoft/plow/service/RndProcessManager.java
@@ -3,6 +3,7 @@ package com.breakersoft.plow.service;
 import java.util.List;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +22,7 @@ import com.breakersoft.plow.rndaemon.RndClient;
 @Component
 public class RndProcessManager {
 
-    private Logger logger = org.slf4j.LoggerFactory.getLogger(RndProcessManager.class);
+    private static final Logger logger = LoggerFactory.getLogger(RndProcessManager.class);
 
     @Autowired
     NodeService nodeService;
