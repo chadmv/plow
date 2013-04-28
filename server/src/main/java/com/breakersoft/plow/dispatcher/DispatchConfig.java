42d423fc4f8101b5ae9fa35516b2f5b03129dbc3
Matt Chambers <mvchambers@me.com>
2013-04-28 15:37:40 +1200

    Allow the dispatcher to look lots of cores for now.

--- server/src/main/java/com/breakersoft/plow/dispatcher/DispatchConfig.java
+++ server/src/main/java/com/breakersoft/plow/dispatcher/DispatchConfig.java
@@ -8,14 +8,14 @@ public class DispatchConfig {
      * Maximum number of processes to start on a single
      * dispatching pass for a given node.
      */
-    public static int MAX_PROCS_PER_JOB = 4;
+    public static int MAX_PROCS_PER_JOB = 16;
 
     /*
      * Maximum number of cores to dispatch from a node per pass.
      * Can actually go over the max, but if you do then dispatching
      * switches to the next job.
      */
-    public static int MAX_CORES_PER_JOB = 4;
+    public static int MAX_CORES_PER_JOB = 16;
 
     /*
      * Set to true once dispatching should start to happen.
