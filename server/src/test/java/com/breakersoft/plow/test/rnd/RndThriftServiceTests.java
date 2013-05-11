a5044a682d21df4f9b9ac43505af114607f20495
Matt Chambers <mvchambers@me.com>
2013-05-11 20:34:41 +1200

    Place holder for RND tests.

--- server/src/test/java/com/breakersoft/plow/test/rnd/RndThriftServiceTests.java
+++ server/src/test/java/com/breakersoft/plow/test/rnd/RndThriftServiceTests.java
@@ -1,22 +1,13 @@
 package com.breakersoft.plow.test.rnd;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 
 import com.breakersoft.plow.test.AbstractTest;
-import com.breakersoft.plow.thrift.RndThriftServiceImpl;
 
 public class RndThriftServiceTests extends AbstractTest {
-
-    @Resource
-    RndThriftServiceImpl rndService;
 
     @Test
     public void test() {
 
     }
-
-
-
 }
