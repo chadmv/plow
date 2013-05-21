7c8b61f87ad3f564b755523fd55fef4f68710126
Justin Israel <jisrael@wetafx.co.nz>
2013-05-21 15:17:01 +1200

    plow.client will set the host from the config file on module init

--- lib/python/plow/client/__init__.py
+++ lib/python/plow/client/__init__.py
@@ -1,1 +1,20 @@
 from plow import *
+
+def _init():
+    import conf
+
+    hosts = conf.get('plow', 'hosts')
+    if not hosts:
+        return
+
+    tokens = hosts.split(',')[0].split(':')
+
+    if len(tokens) == 2 and tokens[1].isdigit():
+        host, port = tokens 
+        set_host(host, int(port))
+    else:
+        host = tokens[0]
+        set_host(host)
+
+_init()
+del _init
