d88451e6226a255157316b35012dc907b3b696fa
sqlboy <mvchambers@me.com>
2013-06-01 17:14:55 +1200

    Added proper annotations.

--- server/src/main/java/com/breakersoft/plow/scheduler/SchedulerServiceImpl.java
+++ server/src/main/java/com/breakersoft/plow/scheduler/SchedulerServiceImpl.java
@@ -1,7 +1,11 @@
 package com.breakersoft.plow.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.breakersoft.plow.dispatcher.dao.ProcDao;
+import com.breakersoft.plow.rnd.thrift.RunningTask;
 
 @Service
 @Transactional
--- server/src/main/java/com/breakersoft/plow/scheduler/dao/SchedulerDaoImpl.java
+++ server/src/main/java/com/breakersoft/plow/scheduler/dao/SchedulerDaoImpl.java
@@ -1,8 +1,11 @@
 package com.breakersoft.plow.scheduler.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.breakersoft.plow.Task;
 import com.breakersoft.plow.dao.AbstractDao;
 
+@Repository
 public class SchedulerDaoImpl extends AbstractDao implements SchedulerDao {
 
 
