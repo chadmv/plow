3cac23cf33a3142943abdea15c1ee109d9d0a852
Matt Chambers <mvchambers@me.com>
2013-06-13 22:46:42 +1200

    An example task query.

---
+++ server/ddl/task_dispatch_query.sql
@@ -0,0 +1,30 @@
+EXPLAIN ANALYZE
+SELECT  
+    task.pk_task,
+    task.pk_layer,
+    task.pk_job,
+    task.str_name, 
+    task.int_ram_min,   
+    layer.int_cores_min,
+    job.pk_project  
+FROM  
+    plow.layer  
+        INNER JOIN  
+    plow.task ON layer.pk_layer = task.pk_layer  
+        INNER JOIN  
+    plow.job ON layer.pk_job = job.pk_job  
+WHERE  
+    layer.pk_job = '8c46b676-ec60-496e-a671-d9012c5be32e'::uuid
+AND  
+    layer.int_cores_min <= 8
+AND  
+    layer.str_tags && '{unassigned}'
+AND  
+    task.int_state = 1
+AND  
+    task.int_ram_min <= 8096
+AND  
+    task.bool_reserved IS FALSE  
+ORDER BY  
+    task.int_task_order, task.int_layer_order ASC  
+LIMIT 10;
