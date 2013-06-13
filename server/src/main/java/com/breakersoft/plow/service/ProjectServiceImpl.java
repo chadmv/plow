d897a48d156ce62f672531d5724ad0b6fef3967f
Matt Chambers <mvchambers@me.com>
2013-06-13 23:36:19 +1200

    Removed unneeded event calls.

--- server/src/main/java/com/breakersoft/plow/service/ProjectServiceImpl.java
+++ server/src/main/java/com/breakersoft/plow/service/ProjectServiceImpl.java
@@ -13,8 +13,6 @@ import com.breakersoft.plow.Project;
 import com.breakersoft.plow.dao.FolderDao;
 import com.breakersoft.plow.dao.ProjectDao;
 import com.breakersoft.plow.event.EventManager;
-import com.breakersoft.plow.event.FolderCreatedEvent;
-import com.breakersoft.plow.event.ProjectCreatedEvent;
 
 @Service
 @Transactional
@@ -34,14 +32,12 @@ public class ProjectServiceImpl implements ProjectService {
         Project project = projectDao.create(title, code);
         Folder folder = createFolder(project, Defaults.FOLDER_DEFAULT_NAME);
         projectDao.setDefaultFolder(project, folder);
-        eventManager.post(new ProjectCreatedEvent(project));
         return project;
     }
 
     @Override
     public Folder createFolder(Project project, String name) {
         Folder folder = folderDao.createFolder(project, name);
-        eventManager.post(new FolderCreatedEvent(folder));
         return folder;
     }
 
@@ -55,33 +51,33 @@ public class ProjectServiceImpl implements ProjectService {
         return projectDao.getAll();
     }
 
-	@Override
-	public void setProjectActive(Project project, boolean value) {
-		projectDao.setActive(project, value);
-	}
-
-	@Override
-    public Folder getFolder(UUID id) {
-    	return folderDao.get(id);
+    @Override
+    public void setProjectActive(Project project, boolean value) {
+        projectDao.setActive(project, value);
     }
 
-	@Override
-	public void setFolderMaxCores(Folder folder, int value) {
-		folderDao.setMaxCores(folder, value);
-	}
+    @Override
+    public Folder getFolder(UUID id) {
+        return folderDao.get(id);
+    }
 
-	@Override
-	public void setFolderMinCores(Folder folder, int value) {
-		folderDao.setMinCores(folder, value);
-	}
+    @Override
+    public void setFolderMaxCores(Folder folder, int value) {
+        folderDao.setMaxCores(folder, value);
+    }
 
-	@Override
-	public void setFolderName(Folder folder, String name) {
-		folderDao.setName(folder, name);
-	}
+    @Override
+    public void setFolderMinCores(Folder folder, int value) {
+        folderDao.setMinCores(folder, value);
+    }
 
-	@Override
-	public void deleteFolder(Folder folder) {
-		folderDao.delete(folder);
-	}
+    @Override
+    public void setFolderName(Folder folder, String name) {
+        folderDao.setName(folder, name);
+    }
+
+    @Override
+    public void deleteFolder(Folder folder) {
+        folderDao.delete(folder);
+    }
 }
