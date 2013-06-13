dae3c72e38a83820e23271448e63379a33cfe158
sqlboy <mvchambers@me.com>
2013-06-13 15:35:10 +1200

    Useful functions for tools, going to have GUI use these modules as well.

---
+++ lib/python/plow/tools/constants.py
@@ -0,0 +1,18 @@
+#
+# Display names
+#
+TIME_NO_TIME = "__-__ __:__:__"
+
+TIME_NO_DURATION = "__:__:__"
+
+TASK_STATES = ["INITIALIZE", 
+               "WAITING",
+               "RUNNING",
+               "DEAD",
+               "EATEN",
+               "DEPEND",
+               "SUCCEEDED"]
+
+JOB_STATES = ["INITIALIZE", 
+              "RUNNING",
+              "FINISHED"]
