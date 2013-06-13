a302ea85d0025c036a902fdb2ef3764bb855e618
Justin Israel <justinisrael@gmail.com>
2013-06-14 11:29:16 +1200

    added getSettings() utility method

--- lib/python/plow/gui/util.py
+++ lib/python/plow/gui/util.py
@@ -3,7 +3,7 @@ import os
 import time
 from datetime import datetime
 
-from manifest import QtGui
+from manifest import QtCore, QtGui
 from plow.tools.util import *
 
 
@@ -13,9 +13,19 @@ def loadTheme():
     QtGui.qApp.setStyleSheet(open(path).read())
 
 
+def getSettings(name, scope=QtCore.QSettings.UserScope, parent=None):
+    settings = QtCore.QSettings(QtCore.QSettings.NativeFormat, 
+                                scope,
+                                "plow", 
+                                name, 
+                                parent)
+    return settings
+
+
 def ask(msg, title='Confirm?', parent=None):
     ret = QtGui.QMessageBox.question(parent, title, msg,
                                      QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel,
                                      QtGui.QMessageBox.Cancel )
    
     return ret == QtGui.QMessageBox.Ok
+
