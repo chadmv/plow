6ee03ac6ad5020cf927160cb8197c6397d739ff1
sqlboy <mvchambers@me.com>
2013-06-14 15:38:27 +1200

    Change the StatsController to MonitorController to not get confused with the stats system.

---
+++ server/src/main/java/com/breakersoft/plow/http/MonitorController.java
@@ -0,0 +1,101 @@
+package com.breakersoft.plow.http;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.breakersoft.plow.Defaults;
+import com.breakersoft.plow.PlowThreadPools;
+import com.breakersoft.plow.monitor.JvmStats;
+import com.breakersoft.plow.monitor.PlowStats;
+import com.google.common.collect.Maps;
+
+/**
+ *
+ * A json dump of the server status.
+ *
+ * @author mchambers
+ *
+ */
+@Controller
+public class MonitorController {
+
+    @Autowired
+    PlowThreadPools plowThreadPools;
+
+    @RequestMapping(value = "/monitor", method = RequestMethod.GET)
+    public void home(HttpServletResponse response) throws IOException {
+
+        final Map<String, Object> result = Maps.newLinkedHashMap();
+        result.put("time", System.currentTimeMillis());
+        result.put("jvm", JvmStats.getJvmStats());
+
+        final Map<String, Object> pools =  Maps.newLinkedHashMap();
+        pools.put("node_dsp", collectThreadPoolStats(plowThreadPools.nodeDispatcherExecutor()));
+        pools.put("proc_dsp", collectThreadPoolStats(plowThreadPools.procDispatcherExecutor()));
+        pools.put("async_command", collectThreadPoolStats(plowThreadPools.stateChangeExecutor()));
+
+        result.put("threadpools", pools);
+
+        final Map<String, Object> general =  Maps.newLinkedHashMap();
+        result.put("general_stats", general);
+
+        final Map<String, Object> dsp_node =  Maps.newLinkedHashMap();
+        dsp_node.put("hit", PlowStats.nodeDispatchHit.get());
+        dsp_node.put("miss", PlowStats.nodeDispatchMiss.get());
+        dsp_node.put("fail", PlowStats.nodeDispatchFail.get());
+        general.put("node_dsp", dsp_node);
+
+        final Map<String, Object> dsp_proc =  Maps.newLinkedHashMap();
+        dsp_proc.put("hit", PlowStats.procDispatchHit.get());
+        dsp_proc.put("miss", PlowStats.procDispatchMiss.get());
+        dsp_proc.put("fail", PlowStats.procDispatchFail.get());
+        general.put("proc_dsp", dsp_proc);
+
+        final Map<String, Object> rnd =  Maps.newLinkedHashMap();
+        rnd.put("ping", PlowStats.rndPingCount.get());
+        rnd.put("task_complete", PlowStats.rndTaskComplete.get());
+        general.put("rnd", rnd);
+
+        final Map<String, Object> procs =  Maps.newLinkedHashMap();
+        procs.put("alloc", PlowStats.procAllocCount.get());
+        procs.put("alloc_fail", PlowStats.procAllocFailCount.get());
+        procs.put("unalloc", PlowStats.procUnallocCount.get());
+        procs.put("unalloc_fail", PlowStats.procUnallocFailCount.get());
+        general.put("procs", procs);
+
+        final Map<String, Object> tasks =  Maps.newLinkedHashMap();
+        tasks.put("started", PlowStats.taskStartedCount.get());
+        tasks.put("started_fail", PlowStats.taskStartedFailCount.get());
+        tasks.put("stopped", PlowStats.taskStoppedCount.get());
+        tasks.put("stopped_fail", PlowStats.taskStoppedFailCount.get());
+        general.put("tasks", tasks);
+
+        final Map<String, Object> jobs =  Maps.newLinkedHashMap();
+        jobs.put("started", PlowStats.jobLaunchCount.get());
+        jobs.put("started_fail", PlowStats.jobLaunchFailCount.get());
+        jobs.put("stopped", PlowStats.jobFinishCount.get());
+        jobs.put("killed", PlowStats.jobKillCount.get());
+        general.put("jobs", jobs);
+
+        Defaults.MAPPER.writeValue(response.getOutputStream(), result);
+    }
+
+    private Map<String,Object> collectThreadPoolStats(ThreadPoolTaskExecutor t) {
+           Map<String,Object> stats = Maps.newHashMap();
+           stats.put("threads", t.getThreadPoolExecutor().getPoolSize());
+           stats.put("active", t.getActiveCount());
+           stats.put("completed", t.getThreadPoolExecutor().getCompletedTaskCount());
+           stats.put("queue", t.getThreadPoolExecutor().getQueue().size());
+           stats.put("capacity", t.getThreadPoolExecutor().getQueue().remainingCapacity());
+           stats.put("largest", t.getThreadPoolExecutor().getLargestPoolSize());
+           return stats;
+    }
+}
