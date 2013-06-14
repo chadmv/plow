a1eab23edbe79805ece45c277a020222aec6f041
Justin Israel <justinisrael@gmail.com>
2013-06-14 14:51:18 +1200

    rndaemon cycles through the list of PLOW_HOSTS connections refs #82

--- lib/python/plow/rndaemon/client.py
+++ lib/python/plow/rndaemon/client.py
@@ -1,23 +1,52 @@
-import conf
+import random
+import logging 
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+import conf
 from rpc import RndServiceApi, RndNodeApi
+
+logger = logging.getLogger(__name__)
 
 
 def getPlowConnection(host=None, port=None):
-    conf_host, conf_port = conf.PLOW_HOSTS[0].split(":")
-    if host is None:
-        host = conf_host
-    if port is None:
-        port = conf_port
-    socket = TSocket.TSocket(host, int(port))
-    transport = TTransport.TFramedTransport(socket)
+    available = [h.split(":") for h in conf.PLOW_HOSTS]
+
+    host_port = random.choice(available)
+    conf_host, conf_port = host_port
+
+    is_custom = host is not None or port is not None
+    host = host if host is not None else conf_host
+    port = port if port is not None else conf_port
+
+    while True:
+        socket = TSocket.TSocket(host, int(port))
+        transport = TTransport.TFramedTransport(socket)
+
+        try:
+            transport.open()
+        except TTransport.TTransportException, e:
+            msg = "Failed to connect to Plow server %s:%s" % (host, port)
+            if is_custom:
+                logger.exception(msg)
+            else:
+                logger.warn(msg)
+        else:
+            break
+        
+        available.remove(host_port)
+        if not available:
+            raise IOError("No available Plow host. Connection attempts all failed")
+
+        host_port = random.choice(available)
+        host, port = host_port   
+
+    logger.debug("Connected to Plow server %s:%s", host, port)
     protocol = TBinaryProtocol.TBinaryProtocolAccelerated(transport)
     service = RndServiceApi.Client(protocol)
-    transport.open()
+
     return (service, transport)
 
 
@@ -26,8 +55,8 @@ def getLocalConnection(port=None):
         port = conf.NETWORK_PORT
     socket = TSocket.TSocket("localhost", port)
     transport = TTransport.TFramedTransport(socket)
+    transport.open()
     protocol = TBinaryProtocol.TBinaryProtocolAccelerated(transport)
     service = RndNodeApi.Client(protocol)
-    transport.open()
     return (service, transport)
 
