9e442f4e7023d6f15a504d2c862f3549088c8a92
sqlboy <mvchambers@me.com>
2013-06-14 14:04:32 +1200

    Cleanups, old files and imports.

--- server/src/main/java/com/breakersoft/plow/dispatcher/StatsServiceImpl.java
+++ server/src/main/java/com/breakersoft/plow/dispatcher/StatsServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
