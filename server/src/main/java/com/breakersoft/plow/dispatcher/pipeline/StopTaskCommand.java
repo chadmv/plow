a6e973f44b7e32406f617bf9d3d94440422d750c
sqlboy <mvchambers@me.com>
2013-06-18 12:08:21 +1200

    Removed "WaitingJob", fixed the StopTaskCommand.

--- server/src/main/java/com/breakersoft/plow/dispatcher/pipeline/StopTaskCommand.java
+++ server/src/main/java/com/breakersoft/plow/dispatcher/pipeline/StopTaskCommand.java
@@ -8,17 +8,21 @@ import com.breakersoft.plow.rnd.thrift.RunTaskResult;
 
 public class StopTaskCommand extends PipelineCommand {
 
-    private RunTaskResult runTaskResult;
+    private final RunTaskResult runTaskResult;
+    private final Task task;
+    private final DispatchProc dispatchProc;
     private PipelineCommandService service;
 
     public StopTaskCommand(RunTaskResult result, Task task, DispatchProc proc, PipelineCommandService service) {
         super(UUID.fromString(result.jobId));
         this.runTaskResult = result;
+        this.task = task;
+        this.dispatchProc = proc;
         this.service = service;
     }
 
     @Override
     public void process() {
-
+        service.stopTask(runTaskResult, task, dispatchProc);
     }
 }
