39f60f46a313f138bbdd46c5ae3aa91f079c9b20
sqlboy <mvchambers@me.com>
2013-06-18 12:08:59 +1200

    Added an AbstractDispatcher to handle common dispatcher methods.

---
+++ server/src/main/java/com/breakersoft/plow/dispatcher/AbstractDispatcher.java
@@ -0,0 +1,37 @@
+package com.breakersoft.plow.dispatcher;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.breakersoft.plow.ExitStatus;
+import com.breakersoft.plow.Signal;
+import com.breakersoft.plow.dispatcher.domain.DispatchProc;
+import com.breakersoft.plow.dispatcher.domain.DispatchResult;
+import com.breakersoft.plow.dispatcher.domain.DispatchTask;
+import com.breakersoft.plow.monitor.PlowStats;
+import com.breakersoft.plow.thrift.TaskState;
+
+public class AbstractDispatcher {
+
+    private static final Logger logger =
+            org.slf4j.LoggerFactory.getLogger(AbstractDispatcher.class);
+
+    @Autowired
+    protected DispatchService dispatchService;
+
+    protected void dispatchFailed(DispatchResult result, DispatchProc proc, DispatchTask task, String message) {
+        logger.error("Unable to dispatch {}/{}, {}", new Object[] {proc, task, message});
+
+        PlowStats.nodeDispatchFail.incrementAndGet();
+        if (task != null) {
+            if (task.started) {
+                dispatchService.stopTask(task, TaskState.WAITING, ExitStatus.FAIL, Signal.ABORTED_TASK);
+            }
+            else {
+                dispatchService.unreserveTask(task);
+            }
+        }
+        dispatchService.deallocateProc(proc, message);
+        result.continueDispatch = false;
+    }
+}
