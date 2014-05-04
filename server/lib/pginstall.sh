2ddcea26ed973ea61b425c518fc644ddc9c2677d
Chad Vernon <chadvernon@gmail.com>
2014-05-04 12:32:46 -0700

    Remove pginstall and associated jar file since it is no longer needed

--- server/lib/pginstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mvn -e install:install-file \
-    -DgroupId=postgresql \
-    -DartifactId=postgresql \
-    -Dversion=9.2-1002.jdbc4 \
-    -Dpackaging=jar \
-    -Dfile=postgresql-9.2-1002.jdbc4.jar
