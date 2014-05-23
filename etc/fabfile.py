b7990f3c2ecc1e7390d46ab535f334cc2822d4bb
Chad Vernon <chad.vernon@camdigital.net>
2014-05-22 18:46:43 -0700

    Fabric and nohup don't work together.  Also get startup errors with daemonize.

--- etc/fabfile.py
+++ etc/fabfile.py
@@ -1,10 +1,10 @@
 import os
-from fabric.api import cd, env, sudo
+from fabric.api import cd, env, run, sudo
 
 env.hosts = [os.environ['PLOW_HOST']]
 env.user = "admin"
 
-distdir = '$PLOW_ROOT/server/dist'
+distdir = os.path.join(os.environ['PLOW_ROOT'], 'server', 'dist')
 
 def stopdb():
     run('ps aux | grep "postgres -D" | grep -v "grep" | awk \'{print $2}\' | xargs kill')
@@ -19,28 +19,9 @@ def restartdb():
     startdb()
 
 
-def startplow():
-    with cd(distdir):
-        run('nohup java -Dplow.cfg.path=%s/resources/plow.properties -jar %s/winstone.jar --webappsDir=%s/webapps --httpPort=8081 > ~/plow_server.log 2>&1&' % (distdir, distdir, distdir))
-
-
 def stopplow():
     run('ps aux | grep "plow.cfg" | grep -v "grep" | awk \'{print $2}\' | xargs kill')
 
 
-def restartplow():
-    stopplow()
-    startplow()
-
-
-def startrndaemon():
-    run('nohup rndaemon > ~/plow_rndaemon.log 2>&1&')
-
-
 def stoprndaemon():
     run('ps aux | grep rndaemon | grep -v "grep" | awk \'{print $2}\' | xargs kill')
-
-
-def restartrndaemon():
-    stoprndaemon()
-    startrndaemon()
